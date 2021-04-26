599366f4a952300a288f518c5bd5a5c78cd4104f
Pamela Hernández <lilpam16@gmail.com>
2021-04-25 23:09:36 -0400

    Agrego JoinVehiculos

--- obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/_Imports.razor.g.cs
@@ -13,112 +13,112 @@ namespace ProyectoFinal
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 1 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 2 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 3 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 4 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 5 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 6 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 7 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 8 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 9 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using ProyectoFinal;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 10 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using ProyectoFinal.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 11 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using ProyectoFinal.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 12 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using MudBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 13 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using ProyectoFinal.Theme;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 14 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 15 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using BlazorInputFile;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 16 "c:\Users\danny\Desktop\ProyectoFinal\_Imports.razor"
+#line 16 "C:\Users\Pamela Hernandez\Desktop\ITLA\5- Quinto Cuatrimestre\Programacion III\ProyectoFinal\_Imports.razor"
 using ProyectoFinal.Services;
 
 #line default
